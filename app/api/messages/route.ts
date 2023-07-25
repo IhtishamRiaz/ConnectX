53ac8a45d866da7694a5b6c3e53c862d13ceea73
Muhammad Ihtisham <riaz4u72nb+github@gmail.com>
2023-07-25 22:28:08 +0500

    new and seen conversation pusher

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -70,7 +70,7 @@ export async function POST(request: Request) {
     updatedConversation.users.map((user) => {
       pusherServer.trigger(user.email!, 'conversation:update', {
         id: conversationId,
-        messages: lastMessage
+        messages: [lastMessage]
       });
     });
 
